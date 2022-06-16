143c4a1da564a6dc3fabc270e982df8df84d5fcc
JanisPlayer <54918417+JanisPlayer@users.noreply.github.com>
2022-06-17 00:55:50 +0200

    Update generator.php

--- generator.php
+++ generator.php
@@ -36,8 +36,9 @@ function open_api_getPostsByAuthor () { //Die Funktion kann später falls benöt
   global $pathtemplate;
   //Maximale Aufrufe 1 alle 5 Minuten.
   if (file_exists($pathtemplate."PostsByAuthor.json")) {  //Datei vorhanden?
-    $file = json_decode(file_get_contents($pathtemplate."PostsByAuthor.json"), true);
-    if ((time() - $file["datum"] >= 300)  ) { //Sind die 5 Minuten abgelaufen?
+    if (time() - filemtime($pathtemplate."PostsByAuthor.json")  >= 300) { //Sind die 5 Minuten abgelaufen? Vielleicht schneller als die komplette Datei zu lesen.
+    //$file = json_decode(file_get_contents($pathtemplate."PostsByAuthor.json"), true);
+    //if ((time() - $file["datum"] >= 300)  ) { //Sind die 5 Minuten abgelaufen?
       //Neue Datei erstellen
       echo "Neue Datei erstellen";
       $json = file_get_contents("https://sds.steemworld.org/feeds_api/getPostsByAuthor/janisplayer");
@@ -51,6 +52,7 @@ function open_api_getPostsByAuthor () { //Die Funktion kann später falls benöt
       } else {
       //Datei lesen
       echo "Datei lesen";
+      $file = json_decode(file_get_contents($pathtemplate."PostsByAuthor.json"), true);
       $jsond = json_decode($file['inhalt'], true);
     }
   } else {
@@ -79,8 +81,101 @@ function read_api(int $i, string $cols) {
   echo isset($jsond);*/
     }
 }
+return $jsond["result"]["rows"][$i][$jsond["result"]["cols"][$cols]];
+}
 
-  return $jsond["result"]["rows"][$i][$jsond["result"]["cols"][$cols]];
+//Wurde dieser Beitrag schon erstellt?
+function exist_site(string $artikel) {
+  $files = scandir("/var/www/html/artikel/", 1);
+  foreach ($files as $key => $value) {
+  if (!strstr($value, '.')) {
+      if ($artikel == $value) {
+          return $value;
+        break;
+      }
+    }
+  }
+  return false;
+}
+
+function gen_site_data(string $permlink) {  //Gibt es diesen Beitrag im Blog?
+//global $jsond; //global
+  $jsond = open_api_getPostsByAuthor();
+  for ($i=0; $i < count($jsond["result"]["rows"]); $i++) {
+    if (read_api($i,"permlink") == $permlink) {
+        $permlink = read_api($i,"permlink");
+        $data['title'] = read_api($i,"title");
+        //Muss verbessert werden.
+        //$data['description'] $jsond["result"]["rows"][$i][$jsond["cols"]["body"]];
+        //$data['keywords'] = $jsond["result"]["rows"][$i][$jsond["cols"]["json_metadata"]];
+        $data['upvote_count'] = read_api($i,"upvote_count");
+        $data['downvote_count'] = read_api($i,"downvote_count");
+        $data['datum'] = read_api($i,"created");
+        $data['category'] = read_api($i,"category");
+        $data['author'] = read_api($i,"author");
+        $data['foldername'] = $permlink;
+        $data['permlink'] = $permlink;
+
+        $json_getPost = file_get_contents("https://sds.steemworld.org/posts_api/getPost/janisplayer/".$permlink);
+        $jsond_getPost = json_decode($json_getPost, true);
+
+        $data['getPost'] = $jsond_getPost;
+
+        $data['body'] = $jsond_getPost["result"]["body"];
+        $data['last_update'] = $jsond_getPost["result"]["last_update"];
+
+        //return $permlink;
+        return $data;
+        break;
+    }
+  }
+  return false;
+}
+
+function gen_site(string $permlink, bool $weiterleitung) { //Seite erstellen.
+global $pathtemplate;
+global $pathtsite;
+
+  $exist_site_bool = false;
+  $exist_site_bool = exist_site($permlink);
+
+  if ($exist_site_bool != false) {
+      $permlink = $exist_site_bool;
+  }
+
+  $gen_site_bool = false;
+  $data;
+  if ($exist_site_bool == false) {
+    $gen_site_bool = gen_site_data($permlink);
+    $data = $gen_site_bool;
+    /*if ($gen_site_bool != false) { Ist eh Blödsinn-
+        $permlink = $gen_site_bool;
+    }*/
+  }
+
+  echo $exist_site_bool? 'true' : 'false';;
+  echo $gen_site_bool? 'true' : 'false';;
+
+  if (($exist_site_bool == false) && ($gen_site_bool != false)) {
+      file_put_contents($pathtemplate."index_".$permlink.".json",json_encode($data));
+      render($pathtemplate.'artikel.php', $pathtsite.$permlink.'/', $data);
+      if ($weiterleitung == true) {
+      echo "Generiert weiterleitung... zu ".'https://heldendesbildschirms.de/artikel/'.$permlink.'/';
+      header('Location: https://heldendesbildschirms.de/artikel/'.$permlink.'/', true, 301);
+      }
+  } else {
+    if ($exist_site_bool != false) {
+      if ($weiterleitung == true) {
+      echo "Generiert weiterleitung... zu ".'https://heldendesbildschirms.de/artikel/'.$permlink.'/';
+      header('Location: https://heldendesbildschirms.de/artikel/'.$permlink.'/', true, 301);
+      }
+      } else {
+        if ($weiterleitung == true) {
+        echo "Seite ungültig weiterleitung... zu ".'https://heldendesbildschirms.de/artikel/';
+        header('Location: https://heldendesbildschirms.de/artikel/');
+        }
+      }
+  }
 }
 
 $data =[
@@ -106,98 +201,31 @@ $pathtsite = '/var/www/html/artikel/';
 $permlink = "";
 
 if (isset($_GET['artikel'])) {
-//Wurde dieser Beitrag schon erstellt?
-$exist_site_bool = false;
-
-  $files = scandir("/var/www/html/artikel/", 1);
-  foreach ($files as $key => $value) {
-  if (!strstr($value, '.')) {
-      if ($_GET['artikel'] == $value) {
-          $exist_site_bool = true;
-          $permlink = $value;
-        break;
-      }
-    }
-  }
-//Gibt es diesen Beitrag im Blog?
-$gen_site_bool = false;
-
-
-if ($exist_site_bool == false) { //Ich habe vergessen das $ zu setzen, juhu 1 Stunde RIP, aber dafür den Code überprüft und andere Fehler gefunden. :D
-
-  //global $jsond; //global
+  gen_site($_GET['artikel'],true);
+}
+if (isset($_POST['genallcontent'])) { //Wird per Javascript aufgerufen.
   $jsond = open_api_getPostsByAuthor();
   for ($i=0; $i < count($jsond["result"]["rows"]); $i++) {
-    if (read_api($i,"permlink") == $_GET['artikel']) {
-        $permlink = read_api($i,"permlink");
-        $data['title'] = read_api($i,"title");
-        //Muss verbessert werden.
-        //$data['description'] $jsond["result"]["rows"][$i][$jsond["cols"]["body"]];
-        //$data['keywords'] = $jsond["result"]["rows"][$i][$jsond["cols"]["json_metadata"]];
-        $data['upvote_count'] = read_api($i,"upvote_count");
-        $data['downvote_count'] = read_api($i,"downvote_count");
-        $data['datum'] = read_api($i,"created");
-        $data['category'] = read_api($i,"category");
-        $data['author'] = read_api($i,"author");
-        $data['foldername'] = $permlink;
-        $data['permlink'] = $permlink;
-
-        $json_getPost = file_get_contents("https://sds.steemworld.org/posts_api/getPost/janisplayer/".$permlink);
-        $jsond_getPost = json_decode($json_getPost, true);
-
-        $data['getPost'] = $jsond_getPost;
-
-        $data['body'] = $jsond_getPost["result"]["body"];
-        $data['last_update'] = $jsond_getPost["result"]["last_update"];
-
-        $gen_site_bool = true;
-    }
+    gen_site(read_api($i,"permlink"), false);
   }
-
-  /*for ($i=0; $i < count($jsond["result"]["rows"]); $i++) {
-    if ($jsond["result"]["rows"][$i][$jsond["result"]["cols"]["permlink"]] == $_GET['artikel']) {
-        $permlink = $jsond["result"]["rows"][$i][$jsond["result"]["cols"]["permlink"]];
-        $data['title'] = $jsond["result"]["rows"][$i][$jsond["result"]["cols"]["title"]];
-        //Muss verbessert werden.
-        //$data['description'] $jsond["result"]["rows"][$i][$jsond["cols"]["body"]];
-        //$data['keywords'] = $jsond["result"]["rows"][$i][$jsond["cols"]["json_metadata"]];
-        $data['upvote_count'] = $jsond["result"]["rows"][$i][$jsond["result"]["cols"]["upvote_count"]];
-        $data['downvote_count'] = $jsond["result"]["rows"][$i][$jsond["result"]["cols"]["downvote_count"]];
-        $data['datum'] = $jsond["result"]["rows"][$i][$jsond["result"]["cols"]["created"]];
-        $data['foldername'] = $permlink;
-        $data['permlink'] = $permlink;
-
-        $json_getPost = file_get_contents("https://sds.steemworld.org/posts_api/getPost/janisplayer/".$permlink);
-        $jsond_getPost = json_decode($json_getPost, true);
-
-        $data['body'] = $jsond_getPost["result"]["body"];
-        $data['last_update'] = $jsond_getPost["result"]["last_update"];
-
-        $gen_site_bool = true;
-    }
-  }*/
 }
 
-echo $exist_site_bool? 'true' : 'false';;
-echo $gen_site_bool? 'true' : 'false';;
-
-if (($exist_site_bool == false) && ($gen_site_bool == true)) {
-    file_put_contents($pathtemplate."index_".$permlink.".json",json_encode($data));
-    render($pathtemplate.'artikel.php', $pathtsite.$permlink.'/', $data);
-    echo "Generiert weiterleitung... zu ".'https://heldendesbildschirms.de/artikel/'.$permlink.'/';
-    header('Location: https://heldendesbildschirms.de/artikel/'.$permlink.'/', true, 301);
+/* für die Hauptseite eine Möglichkeit alle Artikel zu überprüfen.
+ob_start();
+if (isset($_GET['artikel'])) {
+  gen_site($_GET['artikel'],true);
 } else {
-  if ($exist_site_bool == true) {
-    echo "Weiterleitung... zu ".'https://heldendesbildschirms.de/artikel/'.$permlink.'/';
-    header('Location: https://heldendesbildschirms.de/artikel/'.$permlink.'/', true, 301);
-    } else {
-      echo "Seite ungültig weiterleitung... zu ".'https://heldendesbildschirms.de/artikel/';
-      header('Location: https://heldendesbildschirms.de/artikel/');
-    }
+  $jsond = open_api_getPostsByAuthor();
+  for ($i=0; $i < count($jsond["result"]["rows"]); $i++) {
+    gen_site(read_api($i,"permlink"), false);
+  }
 }
-}
+ob_end_clean();
 
-if (isset($_GET['genalljson_pass'])) { //Wird später als Funktion umgeschreiben, die die ganzen Beiträge auf veränderung prüft, last_update, upvote_count, downvote_count.
+include_once './artikel.html'
+*/
+
+/*if (isset($_GET['genalljson_pass'])) { //Wird später als Funktion umgeschreiben, die die ganzen Beiträge auf veränderung prüft, last_update, upvote_count, downvote_count.
   $jsond = open_api_getPostsByAuthor();
   for ($i=0; $i < count($jsond["result"]["rows"]); $i++) {
     $permlink = read_api($i,"permlink");
@@ -222,5 +250,5 @@ if (isset($_GET['genalljson_pass'])) { //Wird später als Funktion umgeschreiben
     $data['last_update'] = $jsond_getPost["result"]["last_update"];
     file_put_contents($pathtemplate."index_".$permlink.".json",json_encode($data));
   }
-}
+}*/
 ?>
