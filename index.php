6e42de9e7d284b332a1c643d72be166cb589e2c0
JanisPlayer <54918417+JanisPlayer@users.noreply.github.com>
2022-06-27 13:23:12 +0200

    fix xss protection. :D

--- index.php
+++ index.php
@@ -83,14 +83,19 @@ function open_api_getPostsByAuthor () { //Die Funktion kann später falls benöt
   return $jsond;
 }
 
-function read_api(int $i, string $cols) {
+function read_api(int $i, string $cols, bool $xss_protection) {
   global $jsond;
   if (isset($jsond) == false) {
   if (!isset($jsond)) {
     $jsond = open_api_getPostsByAuthor();
     }
 }
-return htmlspecialchars($jsond["result"]["rows"][$i][$jsond["result"]["cols"][$cols]], ENT_QUOTES, 'UTF-8');
+
+if ($xss_protection) {
+  return htmlspecialchars($jsond["result"]["rows"][$i][$jsond["result"]["cols"][$cols]], ENT_QUOTES, 'UTF-8');
+}
+return $jsond["result"]["rows"][$i][$jsond["result"]["cols"][$cols]];
+
 }
 
 //Wurde dieser Beitrag schon erstellt?
@@ -112,17 +117,17 @@ function gen_site_data(string $permlink) {  //Gibt es diesen Beitrag im Blog?
 //global $jsond; //global
   $jsond = open_api_getPostsByAuthor();
   for ($i=0; $i < count($jsond["result"]["rows"]); $i++) {
-    if (read_api($i,"permlink") == $permlink) {
-        $permlink = read_api($i,"permlink");
-        $data['title'] = read_api($i,"title");
+    if (read_api($i,"permlink", 0) == $permlink) {
+        $permlink = read_api($i,"permlink", 0);
+        $data['title'] = read_api($i,"title", 1);
         //Muss verbessert werden.
-        $data['description'] = read_api($i,"body");
-        $data['keywords'] = implode(", ", json_decode(read_api($i,"json_metadata"), true)["tags"]);
-        $data['upvote_count'] = read_api($i,"upvote_count");
-        $data['downvote_count'] = read_api($i,"downvote_count");
-        $data['datum'] = read_api($i,"created");
-        $data['category'] = read_api($i,"category");
-        $data['author'] = read_api($i,"author");
+        $data['description'] = read_api($i,"body", 1);
+        $data['keywords'] = implode(", ", json_decode(read_api($i,"json_metadata", 0), true)["tags"]);
+        $data['upvote_count'] = read_api($i,"upvote_count", 0);
+        $data['downvote_count'] = read_api($i,"downvote_count", 0);
+        $data['datum'] = read_api($i,"created", 0);
+        $data['category'] = read_api($i,"category", 1);
+        $data['author'] = read_api($i,"author", 1);
         $data['foldername'] = $permlink;
         $data['permlink'] = $permlink;
 
@@ -223,7 +228,7 @@ if (isset($_GET['artikel'])) {
 } else {
   $jsond = open_api_getPostsByAuthor();
   for ($i=0; $i < count($jsond["result"]["rows"]); $i++) {
-    gen_site(read_api($i,"permlink"), false);
+    gen_site(read_api($i,"permlink", 0), false);
   }
 }
 ob_end_clean();
@@ -240,16 +245,16 @@ include_once './artikel.html';
 if (isset($_GET['genalljson_pass1234'])) { //Wird später als Funktion umgeschreiben, die die ganzen Beiträge auf veränderung prüft, last_update, upvote_count, downvote_count.
   $jsond = open_api_getPostsByAuthor();
   for ($i=0; $i < count($jsond["result"]["rows"]); $i++) {
-    $permlink = read_api($i,"permlink");
-    $data['title'] = read_api($i,"title");
+    $permlink = read_api($i,"permlink", 0);
+    $data['title'] = read_api($i,"title", 1);
     //Muss verbessert werden.
-    $data['description'] = read_api($i,"body");
-    $data['keywords'] = implode(", ", json_decode(read_api($i,"json_metadata"), true)["tags"]);
-    $data['upvote_count'] = read_api($i,"upvote_count");
-    $data['downvote_count'] = read_api($i,"downvote_count");
-    $data['datum'] = read_api($i,"created");
-    $data['category'] = read_api($i,"category");
-    $data['author'] = read_api($i,"author");
+    $data['description'] = read_api($i,"body", 1);
+    $data['keywords'] = implode(", ", json_decode(read_api($i,"json_metadata", 0), true)["tags"]);
+    $data['upvote_count'] = read_api($i,"upvote_count", 0);
+    $data['downvote_count'] = read_api($i,"downvote_count", 0);
+    $data['datum'] = read_api($i,"created", 0);
+    $data['category'] = read_api($i,"category", 1);
+    $data['author'] = read_api($i,"author", 1);
     $data['foldername'] = $permlink;
     $data['permlink'] = $permlink;
 
