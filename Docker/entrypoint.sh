cc7245e5c17ad3d0141521a4c436954d79a3889f
JanisPlayer <54918417+JanisPlayer@users.noreply.github.com>
2022-07-08 16:45:35 +0200

    Update entrypoint.sh

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,4 +1,5 @@
 name="yourname"
+modus=3 //1 Javascirpt_Steem / 2 Javascript PHP / 3 PHP Only
 wget "https://raw.githubusercontent.com/JanisPlayer/Steem-Blog-Engine/main/Docker/docker-compose.yml"
 mkdir blog
 cd blog
@@ -13,6 +14,8 @@ wget "https://raw.githubusercontent.com/cure53/DOMPurify/main/dist/purify.min.js
 wget "https://raw.githubusercontent.com/erusev/parsedown/master/Parsedown.php"
 cp index.css ./templates/index.css
 sed -i "s/janisplayer/$name/g" ./index.php
+sed -i "s/modus = 3;/modus = $modus/g" ./index.php
+
 sed -i "s/janisplayer/$name/g" ./artikel.html
 sed -i "s/janisplayer/$name/g" ./templates/artikel.php
 
